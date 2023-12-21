4299f225d748cbca66fa5eb23b59be1292dcc681
gmccannon <gwm13@uakron.edu>
2023-12-20 20:08:09 -0500

    Add live data fetching

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
 import plotly.graph_objects as go
 import pandas as pd
+from yfinance import download
+from datetime import datetime, timedelta
+import pandas_market_calendars as mcal
 
 app = Flask(__name__)
 
+# Get today's date
+today = datetime.now().date()
+
+# Get the NYSE calendar
+nyse = mcal.get_calendar('XNYS')
+
+#get the date information by filtering non open days
+CANDLE_PERIOD = 30
+MARKET_OPEN_DAYS = [day.date().strftime('%Y-%m-%d') for day in nyse.valid_days(end_date=today, start_date=today - timedelta(days=CANDLE_PERIOD))]
+NUMER_OF_OPEN_DAYS = len(MARKET_OPEN_DAYS)
+
 # Sample stock data
+ticker_info = download("AAPL", period='2mo', interval='1d')
 data = {
-    'Date': ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04'],
-    'Open': [100, 105, 98, 102],
-    'High': [110, 112, 100, 105],
-    'Low': [95, 98, 92, 98],
-    'Close': [105, 100, 95, 100],
+    'Date': MARKET_OPEN_DAYS,
+    'Open': [ticker_info['Open'].iloc[i] for i in range(-1, -NUMER_OF_OPEN_DAYS - 1, -1)],
+    'High': [ticker_info['High'].iloc[i] for i in range(-1, -NUMER_OF_OPEN_DAYS - 1, -1)],
+    'Low': [ticker_info['Low'].iloc[i] for i in range(-1, -NUMER_OF_OPEN_DAYS - 1, -1)],
+    'Close': [ticker_info['Close'].iloc[i] for i in range(-1, -NUMER_OF_OPEN_DAYS - 1, -1)],
 }
 
 df = pd.DataFrame(data)
@@ -26,7 +41,7 @@ def generate_candlestick_chart():
 
     # Customize the layout
     fig.update_layout(
-        title='Stock Candlestick Chart',
+        title='AAPL Candlestick Chart',
         xaxis_title='Date',
         yaxis_title='Price',
         xaxis_rangeslider_visible=False
