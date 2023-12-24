47bdc312e59e522c0303bc61770f3dab4e9e55c6
gmccannon <gwm13@uakron.edu>
2023-12-23 21:20:57 -0500

    Add error handling

--- app.py
+++ app.py
@@ -23,64 +23,72 @@ def generate_candlestick_chart():
     candle_period = 180
     market_open_days = [day.date().strftime('%Y-%m-%d') for day in nyse.valid_days(end_date=today, start_date=today - timedelta(days=candle_period))]
     number_open_days = len(market_open_days)
+    
+    try:
+        # get specific stock price data
+        data = {
+        'Date': market_open_days,
+        'Open': [TICKER_INFO['Open'].iloc[i] for i in range(-number_open_days, 0, 1)],
+        'High': [TICKER_INFO['High'].iloc[i] for i in range(-number_open_days, 0, 1)],
+        'Low': [TICKER_INFO['Low'].iloc[i] for i in range(-number_open_days, 0, 1)],
+        'Close': [TICKER_INFO['Close'].iloc[i] for i in range(-number_open_days, 0, 1)],
+        }
 
-    # get specific stock price data
-    data = {
-    'Date': market_open_days,
-    'Open': [TICKER_INFO['Open'].iloc[i] for i in range(-number_open_days, 0, 1)],
-    'High': [TICKER_INFO['High'].iloc[i] for i in range(-number_open_days, 0, 1)],
-    'Low': [TICKER_INFO['Low'].iloc[i] for i in range(-number_open_days, 0, 1)],
-    'Close': [TICKER_INFO['Close'].iloc[i] for i in range(-number_open_days, 0, 1)],
-    }
+        df = DataFrame(data)
+        df['Date'] = to_datetime(df['Date'])
 
-    df = DataFrame(data)
-    df['Date'] = to_datetime(df['Date'])
+        # Create candlestick chart
+        fig = Figure(data=[Candlestick(x=df['Date'],
+                                            open=df['Open'],
+                                            high=df['High'],
+                                            low=df['Low'],
+                                            close=df['Close'])])
 
-    # Create candlestick chart
-    fig = Figure(data=[Candlestick(x=df['Date'],
-                                          open=df['Open'],
-                                          high=df['High'],
-                                          low=df['Low'],
-                                          close=df['Close'])])
-
-    # Customize the layout
-    fig.update_layout(
-        title= f"{INPUT_TICKER} - {Ticker(INPUT_TICKER).info.get('longName', '')}",
-        xaxis_title='Date',
-        yaxis_title='Price',
-        xaxis_rangeslider_visible=False
-    )
+        # Customize the layout
+        fig.update_layout(
+            title= f"{INPUT_TICKER} - {Ticker(INPUT_TICKER).info.get('longName', '')}",
+            xaxis_title='Date',
+            yaxis_title='Price',
+            xaxis_rangeslider_visible=False
+        )
+    except:
+        return "Invalid ticker"
 
     # Convert the Plotly chart to HTML
     return fig.to_html(full_html=True)
 
 
 def generate_info_table():
-    # Create a Ticker object for the specified stock
-    ticker_object = Ticker(INPUT_TICKER)
+    try:
+        # Create a Ticker object for the specified stock
+        ticker_object = Ticker(INPUT_TICKER)
 
-    foward_eps = ticker_object.info.get('forwardPE', 0)
-    trailing_eps = ticker_object.info.get('trailingEps', 0)
-    pe_ratio = ticker_object.info.get('trailingPE', 0)
-    beta = ticker_object.info.get('beta', 0)
+        foward_eps = ticker_object.info.get('forwardPE', 0)
+        trailing_eps = ticker_object.info.get('trailingEps', 0)
+        pe_ratio = ticker_object.info.get('trailingPE', 0)
+        beta = ticker_object.info.get('beta', 0)
 
-    price_open = TICKER_INFO['Open'].iloc[-1]
-    price_close = TICKER_INFO['Close'].iloc[-1]
-    price_high = TICKER_INFO['High'].iloc[-1]
-    price_low = TICKER_INFO['Low'].iloc[-1]
-    
-    sector = ticker_object.info.get('sector', 'Not available')
-    industry = ticker_object.info.get('industry', 'Not available')
-    market_cap = ticker_object.info.get('marketCap', 0)
-    dividend_yield = ticker_object.info.get('dividendYield', 0)
+        price_open = TICKER_INFO['Open'].iloc[-1]
+        price_close = TICKER_INFO['Close'].iloc[-1]
+        price_high = TICKER_INFO['High'].iloc[-1]
+        price_low = TICKER_INFO['Low'].iloc[-1]
+        
+        sector = ticker_object.info.get('sector', 'Not available')
+        industry = ticker_object.info.get('industry', 'Not available')
+        market_cap = ticker_object.info.get('marketCap', 0)
+        dividend_yield = ticker_object.info.get('dividendYield', 0)
 
 
-    info_table = [
-        [f"Today's Open: {price_open:.2f}", f"Trailing EPS: {trailing_eps}", f"Sector: {sector}"],
-        [f"Today's Close: {price_close:.2f}", f"Foward PE: {foward_eps}", f"Industry: {industry}"],
-        [f"Today's High: {price_high:.2f}", f"Trailing PE: {pe_ratio}", f"Market Cap: {format_large_number(market_cap)}"],
-        [f"Today's Low: {price_low:.2f}", f"Dvidend Yield: {(100*dividend_yield):.2f}%", f"Beta: {beta}"],
-    ]
+        info_table = [
+            [f"Today's Open: ${price_open:.2f}", f"Trailing EPS: {trailing_eps} E/S", f"Sector: {sector}"],
+            [f"Today's Close: ${price_close:.2f}", f"Foward PE: {foward_eps} P/E", f"Industry: {industry}"],
+            [f"Today's High: ${price_high:.2f}", f"Trailing PE: {pe_ratio} P/E", f"Market Cap: ${format_large_number(market_cap)}"],
+            [f"Today's Low: ${price_low:.2f}", f"Dvidend Yield: {(100*dividend_yield):.2f}%", f"Beta: {beta}"],
+        ]
+    except:
+        info_table = [ ['0', '0', '0'], ['0', '0', '0'], ['0', '0', '0'], ['0', '0', '0'] ]
+        return info_table
+
     return info_table
 
 
@@ -93,7 +101,7 @@ def index():
     if request.method == 'POST':
         INPUT_TICKER = request.form['input_value'].upper()
         TICKER_INFO = download(INPUT_TICKER, period='1y', interval='1d')
-
+    
     return render_template('index.html', 
         candlestick_chart=generate_candlestick_chart(), 
         info_table=generate_info_table()
